3631259607e4c44bf818e8400f2c61d65ff53762
FREEKILLER201020 <36477166+FREEKILLER201020@users.noreply.github.com>
2023-01-19 13:09:58 +0300

    Initial Commit

---
+++ ntprog_mobileiosdevtestwork/ntprog_mobileiosdevtestworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ntprog_mobileiosdevtestworkApp.swift
+//  ntprog_mobileiosdevtestwork
+//
+//  Created by Kirill Burchenko on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ntprog_mobileiosdevtestworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
